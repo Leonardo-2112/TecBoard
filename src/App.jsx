5e7bf7136b9bb76fc20e0dd52d41ae0dfd688ac7
Leonardo-2112 <afoncoleonardo21@gmail.com>
2026-08-22 15:44:16 -0300

    feat: add header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
 
+function
+
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <main>
       <header>
-        <img src="/???" alt="" />
+        <img src="/logo.png" alt="" />
       </header>
       <section>
-        <img src="/???" alt="" />
+        <img src="/banner.png" alt="" />
       </section>
     </main>
   )
