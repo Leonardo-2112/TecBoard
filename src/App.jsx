ac0d122e551d3a2bee18b1ce41871f297c17d3bc
Leonardo-2112 <afoncoleonardo21@gmail.com>
2026-08-22 16:39:57 -0300

    feat: add form component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,56 @@
 import './App.css'
 
-function
+//props é um OBJETO
+function TituloFormulario({ children }){
+  return(
+    //Pega o filho de props
+    <h2>{children}</h2>
+  )
+}
+
+function CampoDeFormulario({ children }){
+  return(
+    <h2>
+      {children}
+    </h2>
+  )
+}
+
+function Label({ children, htmlFor }){
+  return(
+    <label htmlFor={htmlFor}>
+      {children}
+    </label>
+  )
+}
+
+function CampoDeEntrada(props){
+  //Espalha todas as propriedades de props
+  return <input {...props}/>
+}
+
+function FormularioDeEvento(){
+  return(
+    <form className='form-evento'>
+      {/* Passa o elemento para o parametro componente(função) */}
+      <TituloFormulario>
+        Preencha para criar um evento:
+      </TituloFormulario>
+
+      <CampoDeFormulario>
+        <Label htmlFor="nome">
+          Qual é o nome do evento?
+        </Label>
+        
+        <CampoDeEntrada
+          type="text" 
+          id='nome' 
+          placeholder='Summer dev hits'
+        />
+      </CampoDeFormulario>
+    </form>
+  )
+}
 
 function App() {
 
@@ -12,6 +62,7 @@ function App() {
       <section>
         <img src="/banner.png" alt="" />
       </section>
+      <FormularioDeEvento />
     </main>
   )
 }
