ef8e2db2107d0bb89cad560b9f69948d088c9405
Leonardo-2112 <afoncoleonardo21@gmail.com>
2026-08-22 15:24:03 -0300

    Primeiro commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <main>
+      <header>
+        <img src="/???" alt="" />
+      </header>
+      <section>
+        <img src="/???" alt="" />
+      </section>
+    </main>
+  )
+}
+
+export default App
