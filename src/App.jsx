dd0069c7b41d972b0c64d10a722c7f2226227699
Leonardo-2112 <afoncoleonardo21@gmail.com>
2026-08-22 16:58:06 -0300

    refactor: reorganize form project structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,6 @@
 import './App.css'
-
-//props é um OBJETO
-function TituloFormulario({ children }){
-  return(
-    //Pega o filho de props
-    <h2>{children}</h2>
-  )
-}
-
-function CampoDeFormulario({ children }){
-  return(
-    <h2>
-      {children}
-    </h2>
-  )
-}
-
-function Label({ children, htmlFor }){
-  return(
-    <label htmlFor={htmlFor}>
-      {children}
-    </label>
-  )
-}
-
-function CampoDeEntrada(props){
-  //Espalha todas as propriedades de props
-  return <input {...props}/>
-}
-
-function FormularioDeEvento(){
-  return(
-    <form className='form-evento'>
-      {/* Passa o elemento para o parametro componente(função) */}
-      <TituloFormulario>
-        Preencha para criar um evento:
-      </TituloFormulario>
-
-      <CampoDeFormulario>
-        <Label htmlFor="nome">
-          Qual é o nome do evento?
-        </Label>
-        
-        <CampoDeEntrada
-          type="text" 
-          id='nome' 
-          placeholder='Summer dev hits'
-        />
-      </CampoDeFormulario>
-    </form>
-  )
-}
-
+import { FormularioDeEvento } from './componentes/FormularioDeEvento'
 function App() {
-
   return (
     <main>
       <header>
---
+++ src/componentes/CampoDeEntrada/index.jsx
@@ -0,0 +1,4 @@
+export function CampoDeEntrada(props){
+  //Espalha todas as propriedades de props
+  return <input {...props}/>
+}
