3fa1201953f82554166ca8f58c052cd1dc6e36c3
Leonardo-2112 <afoncoleonardo21@gmail.com>
2026-08-23 16:56:18 -0300

    style: improve card component styling

--- src/componentes/CardEvento/index.jsx
+++ src/componentes/CardEvento/index.jsx
@@ -8,7 +8,7 @@ export function CardEvento({ evento }){
                 <p className='tag'>
                     {evento.tema.nome}
                 </p>
-                <p>
+                <p className='data'>
                     {evento.data.toLocaleDateString('pt-br')}
                 </p>
                 <h4 className='titulo'>
